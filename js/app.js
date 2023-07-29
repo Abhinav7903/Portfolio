9b3f1e76526a9899cdcf2146a25ff67910f13b96
Abhinav Ashish <55495151+Abhinav7903@users.noreply.github.com>
2023-07-29 07:48:15 +0530

    Particle js fix

--- js/app.js
+++ js/app.js
@@ -4,14 +4,14 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 60,
+        "value": 80,
         "density": {
           "enable": true,
           "value_area": 800
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#ff0000"
       },
       "shape": {
         "type": "circle",
@@ -51,7 +51,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#ff0ff0",
         "opacity": 0.4,
         "width": 1
       },
